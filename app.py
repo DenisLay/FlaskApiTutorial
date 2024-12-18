08c2e39f6cf369bc9b6587521889f5e073c8a6d9
Denis Kobzar <laycode00700@gmail.com>
2024-12-18 10:48:14 +0200

    chane port and disable tls

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def create_app(name, debug):
     db.init_app(app)
 
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     app.config['MAIL_USERNAME'] = os.environ["email_link"]
     app.config['MAIL_PASSWORD'] = os.environ["email_password"]
