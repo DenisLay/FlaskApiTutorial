f3d52ca62b40418f9b56c9128036992a239ea1a3
Denis Kobzar <laycode00700@gmail.com>
2024-12-18 10:44:06 +0200

    use ssl for mail

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def create_app(name, debug):
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USE_SSL'] = True
     app.config['MAIL_USERNAME'] = os.environ["email_link"]
     app.config['MAIL_PASSWORD'] = os.environ["email_password"]
     app.config['MAIL_DEFAULT_SENDER'] = os.environ["email_link"]
